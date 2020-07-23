b8c03e0c0cef1a32b92f68cd22bd769218c31a00
FelipeMaxRosa <felipemax.suporteti@gmail.com>
2020-07-23 09:38:01 -0300

    commit inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import GroupCards from "./components/GroupCards";
+import "./App.css";
+import ShopCar from "./components/ShopCar";
+import api from "./services/api";
+
+interface Pokemon {
+  id: number;
+  name: string;
+  url: string;
+  image_url: string;
+}
+
+interface SelectedPokemon {
+  id: number;
+  name: string;
+  price: number;
+}
 
 function App() {
+  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+  const [selectedPokemon, setSelectedPokemon] = useState<SelectedPokemon[]>([]);
+
+  useEffect(() => {
+    const getFirePokemon = async () => {
+      const firePokemon = await api.get("type/fire");
+
+      const filteredPokemon = firePokemon.data.pokemon.map(
+        (poke: any, index: number): Pokemon => {
+          return {
+            id: index + 1,
+            name: poke.pokemon.name.toUpperCase(),
+            url: poke.pokemon.url,
+            image_url: "",
+          };
+        }
+      );
+
+      setPokemon(filteredPokemon);
+    };
+
+    getFirePokemon();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div className="group">
+        <GroupCards data={pokemon} />
+        <ShopCar items={selectedPokemon} />
+      </div>
     </div>
   );
 }
